3e567f9e279222b590fed67a55871a266e5bd1fe
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2022-11-21 11:14:52 +0530

    Create classInstanceCreation.js

---
+++ classInstanceCreation.js
@@ -0,0 +1,16 @@
+
+// creating class instance in JavaScript
+class User{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const user1 = new User("Saikumar", 22);
+console.log(user1);         // User Object with name, age
+console.log(user1.name);    // Saikumar
+console.log(user1.age);     // 22
+
+
+
