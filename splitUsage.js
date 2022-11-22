9b525a7961a59560e67d79015236ffb86c2ea7b1
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2022-11-22 11:12:08 +0530

    Create splitUsage.js

---
+++ splitUsage.js
@@ -0,0 +1,8 @@
+// split usage in js
+
+let x = "How are you";
+let splitString = x.split(" "); // split function takes a delimeter and splits on the condition
+console.log(splitString); // [How, are, you]
+
+
+
