7e3d20127cb8b996c4e5f4339c3bed4e98038fa6
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2022-11-22 11:12:45 +0530

    Create trimUsage.js

---
+++ trimUsage.js
@@ -0,0 +1,12 @@
+// trim usage in js
+
+let x = "   How are you     ";
+console.log(x.length);          // 19
+console.log(x);                 //   How are you     
+let trimString = x.trim();      // trim function removes the first and last spaces of the string
+console.log(trimString.length); // 11
+console.log(trimString);    // How are you
+
+
+
+
