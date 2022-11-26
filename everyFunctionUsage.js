d083bf57a9990fa58da711823753ced376a788e8
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2022-11-26 19:35:10 +0530

    Create everyFunctionUsage.js

---
+++ everyFunctionUsage.js
@@ -0,0 +1,10 @@
+// every function tests whether each element is passing the condition or not
+// it returns a boolean true/false
+
+let numbers = [1, 2, 3, 4];
+const result = numbers.every((number) => number > 0);
+
+console.log(result); // true
+
+
+
