2a7b1de41a8d9b8fb4e4979b5b4011c10cec351b
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2022-11-27 22:27:43 +0530

    Create someFunctionUsage.js

---
+++ someFunctionUsage.js
@@ -0,0 +1,9 @@
+// some function tests whether atleast one element passed the provided function
+
+const numbers = [1, 2, 3, 4, 5];
+const result = numbers.some((number) => number === 4);
+
+console.log(result); // true
+
+
+
