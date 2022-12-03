97bf95c4906801810e8b9cb20ae13b2c41b8d972
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2022-12-03 16:42:58 +0530

    Create includesFunction.js

---
+++ includesFunction.js
@@ -0,0 +1,11 @@
+// includes function usage in strings
+// includes function searches for the given sequence of characters in the string
+// not individual characters
+
+const name = "Saikumar";
+
+console.log(name.includes("ar"));   // true
+console.log(name.includes("sk"));   // false
+
+
+
