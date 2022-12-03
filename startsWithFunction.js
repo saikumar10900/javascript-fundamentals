0335b7a246d5df8b756adcf6964ce63a84438645
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2022-12-03 16:43:34 +0530

    Create startsWithFunction.js

---
+++ startsWithFunction.js
@@ -0,0 +1,10 @@
+// startsWith function usage in strings
+// this function returns a boolean value if the given string starts with given input or not
+
+const name = "Saikumar";
+
+console.log(name.startsWith("Sa"));
+console.log(name.startsWith("ar"));
+
+
+
