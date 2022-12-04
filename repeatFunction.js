9898b2f009135d726007d2101cbcf9dcccdf1a0b
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2022-12-04 16:20:46 +0530

    Create repeatFunction.js

---
+++ repeatFunction.js
@@ -0,0 +1,10 @@
+// repeat function usage with strings
+// this function takes two args 1. the string 2. integer of how many times it has to repeat
+
+const name = "Saikumar";
+
+const repeatedName = name.repeat(3);
+console.log(repeatedName);  // SaikumarSaikumarSaikumar
+
+
+
