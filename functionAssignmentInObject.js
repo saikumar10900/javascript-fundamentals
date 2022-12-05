e7b206b685220d8b4f3991a0d86c1d4be27326fa
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2022-12-05 21:19:10 +0530

    Create functionAssignmentInObject.js

---
+++ functionAssignmentInObject.js
@@ -0,0 +1,15 @@
+// function assignment in Object
+
+const obj = {
+    name: "Saikumar",
+    age: 22,
+    wish(){
+        return "Hi";
+    }
+};
+
+console.log(obj.wish);      // prints the wish function code
+console.log(obj.wish());    // Hi
+
+
+
