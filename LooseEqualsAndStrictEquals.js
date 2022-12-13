e5e378ed4e79f22b4d5341eba5c5f3420b705613
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2022-12-13 21:44:07 +0530

    Create LooseEqualsAndStrictEquals.js

---
+++ LooseEqualsAndStrictEquals.js
@@ -0,0 +1,8 @@
+// === performs strict comparision between the variables
+// it checks for data type.
+
+const number = 10;
+const alpha = "10";
+
+console.log(number == alpha);       // true
+console.log(number === alpha);      // false
