96b863ad4ced2ec0e4d4d2998c86adb1bf9b192f
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-01-12 20:13:06 +0530

    Update AsyncAwait.js

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -1,7 +1,7 @@
  const a = async () => {
- const response = await fetch('URL');
- const b = await response.json();
- console.log(b);
+  const response = await fetch('URL');
+  const b = await response.json();
+  console.log(b);
 }
 
 a()
