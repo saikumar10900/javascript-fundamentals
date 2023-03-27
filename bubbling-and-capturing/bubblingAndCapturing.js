a499d52ae545f6066d3caced9f38dd84f40ff42a
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-03-27 19:48:38 +0530

    Update bubblingAndCapturing.js

--- bubbling-and-capturing/bubblingAndCapturing.js
+++ bubbling-and-capturing/bubblingAndCapturing.js
@@ -1,3 +1,7 @@
+// if you don't provide any boolean value true/false to the event listener, by default it will take false which means it performs bubbling.
+// to justify the below example if you click on child element the output will be like "Parent Clicked".
+
+
 document.getElementById("grandparent").addEventListener('click', () => {
     console.log("Grandparent Clicked!");
 }, false);
