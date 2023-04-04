1024b6b541117dc8c52fee25c52fce86addf9f4e
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-04-04 21:15:37 +0530

    Update FunctionDeclaration.js

--- FunctionDeclaration.js
+++ FunctionDeclaration.js
@@ -1,3 +1,9 @@
+// all function declarations will be hoisted when the code is run
+// we can access the function before declaration.
+
+
+console.log(sayHello());    // Hello
+
 function sayHello(){
     return "Hello";
 }
