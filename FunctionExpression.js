4737d829815100f3638dbba934fce4b0a518f7a0
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-04-05 18:38:27 +0530

    Update FunctionExpression.js

--- FunctionExpression.js
+++ FunctionExpression.js
@@ -1,3 +1,9 @@
+// function expression looks similar to a variable initialisation.
+// function expression doesn't hoist like function declaration.
+// JS engine treats them as variables and keeps undefined as the value.
+
+
+
 const sayHello = function(){
     return "Hello";
 };
