28228b4cdb10647e35aabe3610300ff900d11a1c
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-04-06 09:13:08 +0530

    Update blockScope.js

--- blockScope.js
+++ blockScope.js
@@ -1,4 +1,6 @@
 // block scope
+// let and const declarations are block scoped, So we can't use them outside the block in which they are defined.
+// Using let and const wherever possible, it makes not to fall in unnecessary errors.
 
 let a = 10;
 if (a > 5) {
