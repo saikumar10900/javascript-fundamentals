5bec62104de237e7148fde70a61c3f5b3e7dddf6
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-04-08 08:52:27 +0530

    Update defaultParameters.js

--- defaultParameters.js
+++ defaultParameters.js
@@ -1,3 +1,8 @@
+// we can use default parameters if the function call doesn't carry any arguments.
+// we can define default parameters like below.
+// if the function call send arguments, the default params will get replaced by the args send.
+
+
 function greet(greeting = "Hello"){
     console.log(greeting + " Saikumar");
 }
