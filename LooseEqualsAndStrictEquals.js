c9bcbeb8ccc5234f85929d91513d984ab70ea1ca
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-04-10 08:08:17 +0530

    Update LooseEqualsAndStrictEquals.js

--- LooseEqualsAndStrictEquals.js
+++ LooseEqualsAndStrictEquals.js
@@ -1,5 +1,5 @@
-// === performs strict comparision between the variables
-// it checks for data type.
+// === performs strict comparision between the variables including values and data types.
+// == performs comparision only with the values of both variables
 
 const number = 10;
 const alpha = "10";
