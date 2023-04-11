5838e8217086aa30b18b956069444aabb0fb7d3a
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-04-11 08:18:32 +0530

    Create inversion-of-control-pyramid-of-doom-prevention.js

---
+++ inversion-of-control-pyramid-of-doom-prevention.js
@@ -0,0 +1,24 @@
+// Problems we ofen face with callback functions
+// - Callback Hell - having callback function inside another callback function inside another callback function
+// - Inversion of control - Having a lot callback function inside functions
+    // may create a situation where the develop might loose over the control of callback function
+    
+const cart = ['bags', 'books', 'laptops']
+
+createOrder(cart, function(orderId){
+    showOrderDetails(orderId, function(paymentInfo){
+        proceedToPayment(paymentInfo, function(remaniningBalance){
+            updateWallet(remaniningBalance);
+        });
+    });
+});
+
+// the above will turn into below using promise chaining to avoid inversion of control
+
+createOrder(cart).then(function(orderId){
+    return showOrderDetails(orderId)
+}).then(function(paymentInfo){
+    return proceedToPayment(paymentInfo)
+}).then(function(remaniningBalance){
+    return updateWallet(remaniningBalance)
+})
