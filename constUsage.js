307c46757b406c647a08ec6c4cecac7ecf79adac
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-04-15 08:31:13 +0530

    Update constUsage.js

--- constUsage.js
+++ constUsage.js
@@ -1,3 +1,7 @@
+// const declarations are hoisted in the temporal deadzone for the time being.
+// we can't access any variables in the temporal deadzone.
+// let and const variables doesn't get attached to the window object, means we can't access them in the console window by this.variable or window.variable.
+
 const a = 10;
 a = 20;   // throws an error
 
