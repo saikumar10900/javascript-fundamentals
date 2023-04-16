9f985c11029d580fae1abe14d9520a37a462b636
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-04-16 12:43:26 +0530

    Update flatUsage.js

--- flatUsage.js
+++ flatUsage.js
@@ -1,4 +1,5 @@
 // flat usage in javascript
+// flat function takes an array of arrays and return an new array by combining all elements in the sub arrays.
 
 const a1 = [0, 1, 2, [3, 4]];
 const a2 = [0, 1, 2, [[3, 4]]];
