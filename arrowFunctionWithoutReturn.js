572bebafe89cb2ae8df1390618f5cc5532c088d6
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-04-17 08:49:29 +0530

    Update arrowFunctionWithoutReturn.js

--- arrowFunctionWithoutReturn.js
+++ arrowFunctionWithoutReturn.js
@@ -1,3 +1,11 @@
+// arrow function is introduced in ES6 module and the syntax of it is quite cleaner and simpler.
+// if there are no compound statements, we don't need to return values explicitly.
+// if there is only one argument to be passed, we don'e need to use paranthesis around the args
+
+
+const sayHello = msg => `${} Saikumar`;
+console.log(sayHello("Hi"); // Hi Saikumar
+
 const sum = (a, b) => a + b;
 console.log(sum(10, 20)); // 30
 
