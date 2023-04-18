4c545c8b3497c0447977ad860e366617f136f737
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-04-18 08:56:10 +0530

    Update thisContext.js

--- thisContext.js
+++ thisContext.js
@@ -1,16 +1,25 @@
-console.log(this);
+// the context of this keyword works a kind of different in each scenario
+// the context is completely dependent on where the function call is being made.
+
+
+console.log(this);  // window
 
 function hi(){
-    console.log(this);
+    console.log(this);  // window
+}
+
+const hello = () => {
+    console.log(this)   // window
 }
 
 const obj = {
     name: "saikumar",
     welcome: function hello(){
-        console.log(this);
+        console.log(this);  // Object
     }
 };
 
 hi();
+hello();
 obj.welcome();
 
