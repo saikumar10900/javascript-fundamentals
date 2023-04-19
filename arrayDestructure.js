c7d750d3b4671f9eda7eea595ecf9c5693955a94
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-04-19 08:20:51 +0530

    Update arrayDestructure.js

--- arrayDestructure.js
+++ arrayDestructure.js
@@ -1,3 +1,7 @@
+// spread operator always be used as the last argument to receive the remaining values
+// here, 1 will be assigned to x and the remaining [2, 3] array will be in z
+
+
 function destructureArray(a){
     let [x, ...z] = a;
     return x;
