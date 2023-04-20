70fb8884e00cb38de513839382dd3dd241c2e069
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-04-20 08:26:02 +0530

    Update currying.js

--- currying.js
+++ currying.js
@@ -1,3 +1,7 @@
+// currying transforms the function into nested functions to achieve different output with the same function.
+// here multiply function is returning an another function by forming a closure with its parent function.
+// 2 will be a, 3 will be b here
+
 function multiply (a) {
   return function(b){
     return a * b;
