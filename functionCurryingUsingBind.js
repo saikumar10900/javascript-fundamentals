710bd31fcc9c9dc9f47b56758ae26ba7f75a32f3
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-04-22 08:23:47 +0530

    Update functionCurryingUsingBind.js

--- functionCurryingUsingBind.js
+++ functionCurryingUsingBind.js
@@ -1,4 +1,8 @@
 // Currying using bind function
+// Here, at line 11, we are binding the multiply method with the context and 1 arg. So, will get multiply function copy with 2 as the value to a
+// line 13, we are calling the callback function returned in the line 11 with arg 3, it will be set to b and our output will be 6
+
+
 
 const multiply = function(a, b){
     console.log(a * b);     // 6
