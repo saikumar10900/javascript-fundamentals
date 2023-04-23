02e952636e569cb348142482c3002b7b6e2664cf
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-04-23 10:07:20 +0530

    Update shortestJavascript.js

--- shortestJavascript.js
+++ shortestJavascript.js
@@ -1,3 +1,5 @@
+// even if the js file doesn't have any code in it, still the browser does it part of initializing the memory component for it and it does have window and this object.
+
 console.log(this);              // window object
 console.log(window);            // window object
 console.log(this === window);   // true
