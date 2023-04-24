1429c3f1d0336d7ae503d10b223cfa7087584e2e
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-04-24 09:15:16 +0530

    Update blockScope.js

--- blockScope.js
+++ blockScope.js
@@ -10,4 +10,10 @@ if (a > 5) {
 console.log(z); //  ReferenceError{"z is not defined"}
 
 
+for(let i = 0; i <= 5; i++){
+   ....
+   console.log(i);  // 0, 1, 2, 3, 4, 5
+}
+console.log(i); // Reference error
 
+// let declaration is block scoped, So we can't use outside of for loop
