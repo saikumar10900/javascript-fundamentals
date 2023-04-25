cd3f74aff01c278d9f80d3fc125f0c3bae6ad185
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-04-25 19:57:12 +0530

    Update AsyncAwait.js

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -1,4 +1,6 @@
 // Async await blocks the main thread and doesn't move forward in the code flow until the promise is resolved/rejected
+// line 7 waits until the line 6 finishes getting the data
+// line 8 executes only after completion of line 7.
 
  const a = async () => {
   const response = await fetch('URL');
