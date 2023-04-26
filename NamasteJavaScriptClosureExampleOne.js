e5e0abcbfe223eac90b635d6d115c5eb5c6e6061
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2023-04-26 08:31:07 +0530

    Update NamasteJavaScriptClosureExampleOne.js

--- NamasteJavaScriptClosureExampleOne.js
+++ NamasteJavaScriptClosureExampleOne.js
@@ -1,3 +1,8 @@
+// closure - function bundled with its lexical environment.
+// here, the function inner will form a closure with it's parent outer and grand parent outest as the function is using the variables a and b.
+// even though the function is returned from the outest function, the function inner will still remember the values of a and b as it forms closure.
+
+
 function outest(){
     var a = 1;
     return function outer(){
